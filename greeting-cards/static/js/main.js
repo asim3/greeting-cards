0c3035c77958f8ba02dc6d18885862a2aabb703a
asim3 <asimwebapps@gmail.com>
2021-04-01 00:12:46 +0300

    added: drawImage

--- greeting-cards/static/js/main.js
+++ greeting-cards/static/js/main.js
@@ -1,30 +1,39 @@
-function get_input_text() {
-    return document.getElementById('card_name').value
-}
+$(function () {
+    function get_input_text() {
+        return document.getElementById('card_name').value
+    }
 
 
-function draw(canvas_id) {
-
-    var ctx = document.getElementById(canvas_id).getContext('2d');
-    ctx.font = '48px Cairo';
-    var x = 500
-    var y = 400
-    ctx.fillText(get_input_text(), x, y);
-}
-
-
-function download_card(canvas_id) {
-    var canvas = document.getElementById(canvas_id);
-    var data_URL = canvas.toDataURL();
-
-    const file_name = 'بطاقة المعايدة'
-    const element = document.createElement('a')
-    element.setAttribute("href", data_URL)
-    element.setAttribute("download", file_name + ".png")
-    document.body.appendChild(element)
-    element.click()
-    element.remove()
-}
+    function draw() {
+        canvas_id = 'card_canvas'
+        var ctx = document.getElementById(canvas_id).getContext('2d');
+        var image = document.getElementById("canvas_image");
+        ctx.drawImage(image, 0, 0);
+        ctx.fillStyle = '#FFFFFF';
+        ctx.font = '28px Cairo';
+        var x = 450
+        var y = 550
+        ctx.fillText(get_input_text(), x, y);
+    }
 
 
-draw('card_canvas')
+    function download_card() {
+        canvas_id = 'card_canvas'
+        var canvas = document.getElementById(canvas_id);
+        var data_URL = canvas.toDataURL();
+
+        const file_name = 'بطاقة المعايدة'
+        const element = document.createElement('a')
+        element.setAttribute("href", data_URL)
+        element.setAttribute("download", file_name + ".png")
+        document.body.appendChild(element)
+        element.click()
+        element.remove()
+    }
+
+
+    $('#save').on('click', download_card)
+    $('#card_name').on('keyup', draw)
+
+    draw()
+});
